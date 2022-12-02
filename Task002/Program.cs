27f9948dce3da6e2e1bbb22b65aadf46ba823a49
Valeria_Aleksakhina <Virago400@mail.ru>
2022-12-02 11:50:36 +0400

    added task 002

---
+++ Task002/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+int num = new Random().Next(100, 1000);
+//int num = 999;
+int a1 = num/100;
+int a2 = num%10;
+
+//можно так
+//int result = a1*10+a2;
+//Console.WriteLine(result);
+
+// или так
+Console.Write(a1);
+Console.Write(a2);
