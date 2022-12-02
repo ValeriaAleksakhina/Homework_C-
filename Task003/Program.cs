97ddcf41f6e0bde823385a70dd12bbb00daf1272
Valeria_Aleksakhina <Virago400@mail.ru>
2022-12-02 12:18:26 +0400

    усложнили задачу

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -2,18 +2,37 @@
 // и выводить, является ли второе число кратным первому. 
 //Если число 2 не кратно числу 1, то программа выводит остаток от деления.
 
-Console.WriteLine("Введите число a1");
-int a1 = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Введите число b1");
-int b1 = int.Parse(Console.ReadLine());
-
-if(b1%a1 == 0)
+try
 {
-    Console.WriteLine("Число 2 кратно числу 1");
+
+    Console.WriteLine("Введите число a1");
+    int a1 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите число b1");
+    int b1 = int.Parse(Console.ReadLine());
+
+    if (a1 > b1)
+    {
+        int c = a1;
+        a1 = b1;
+        b1 = c;
+    }
+
+    if (b1 % a1 == 0)
+    {
+        Console.WriteLine($"Число {b1} кратно числу {a1}");
+    }
+    else
+    {
+        int result = b1 % a1;
+        Console.WriteLine($"Число некратно. Остаток равен {result}");
+    }
 }
-else
+
+catch
 {
-    int result = b1%a1;
-    Console.WriteLine($"Число некратно. Остаток равен {result}");
+    Console.WriteLine("Данные не верны");
 }
+
+
+
