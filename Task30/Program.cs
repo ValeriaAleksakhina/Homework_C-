67a537953b6b6b9cfcd2a002d8f50c425f4c2c67
Valeria_Aleksakhina <Virago400@mail.ru>
2022-12-09 12:57:28 +0400

    added method

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -2,6 +2,41 @@
 // заполненный нулями и единицами в случайном порядке
 // [1 0 1 1 1 0 1 1]
 
+
+//Console.WriteLine("Введите длину массива: ");
+//nt N = int.Parse(Console.ReadLine());
+
+// void massive(int dlina)
+//{
+  //  int[] array = new int[dlina];
+  //  int i = 0;
+
+   // while (i<dlina)
+    //{
+   //     int K = new Random().Next(0,2);
+   // array[i] = K;
+  //  if (i == 0)
+  //  {
+   // Console.Write("[" + array[i] + " , ");
+  //  }
+ //   if (i == 7)
+  //  {
+  //  Console.Write(array[i] + " ] ");
+  //  }
+  //  if (i>0 && i<7)
+  //  {
+   //    Console.Write(array[i] + " , "); 
+  //  }
+  //  i++;
+ //   }
+//}
+
+
+
+
+
+
+
 int [] array = new int [8];
 
 int i = 0;
@@ -10,7 +45,18 @@ while (i<=7)
 {
     int K = new Random().Next(0,2);
     array[i] = K;
-    Console.Write(array[i] + " ");
+    if (i == 0)
+    {
+    Console.Write("[" + array[i] + " , ");
+    }
+    if (i == 7)
+    {
+    Console.Write(array[i] + " ] ");
+    }
+    if (i>0 && i<7)
+    {
+       Console.Write(array[i] + " , "); 
+    }
     i++;
 }
 
