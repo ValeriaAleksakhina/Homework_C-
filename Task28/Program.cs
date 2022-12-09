ce19d6f4f6b806e3ba9874dddef4c7536c301ca5
Valeria_Aleksakhina <Virago400@mail.ru>
2022-12-09 12:18:59 +0400

    added task 28

---
+++ Task28/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программу, кот принимает на вход число N
+// и выдает произведение чисел от 1 до N
+
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+void proizv(int num) 
+{
+    int i = 1;
+    int proizvedenie = 1;
+    while (i  != N+1)
+    {
+        proizvedenie *= i;
+        i++;
+    }
+    Console.Write($"Произведение равно: {proizvedenie}");
+}
+proizv(N);
