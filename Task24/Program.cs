cda9774ff76e4f7607adc28fe9e0172618e79bab
Valeria_Aleksakhina <Virago400@mail.ru>
2022-12-09 11:22:11 +0400

    added task 24

---
+++ Task24/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напиши программу, кот принимает на вход число А 
+// и выдает сумму чисел от 1 до А
+// 7 - 28
+// 4 - 10
+// 8- 36
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма чисел = {GetSumNums(num)}");
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while(number>0)
+    {
+        Console.WriteLine($"{number}");
+        sum +=number; // sum = sum+number
+        number--;
+    }
+    return sum;
+}
