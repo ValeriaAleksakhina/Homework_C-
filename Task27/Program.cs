d53d3d77830e48f024cf2c32d4d3929d585feaa8
Valeria_Aleksakhina <Virago400@mail.ru>
2022-12-11 10:38:44 +0400

    added Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Введите число : ");
+int num = int.Parse(Console.ReadLine());
+
+void SumNums(int num) 
+{
+
+    int Summ = 0;
+    while (num != 0)
+    {
+        Summ += num%10;
+        num/=10;
+    }
+    Console.Write($"Сумма чисел: {Summ}");
+}
+SumNums(num);
+
