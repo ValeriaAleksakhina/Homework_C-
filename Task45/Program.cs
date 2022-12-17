fc8e88d6c2a2c5327ade4a80aabf89b0496edce5
Valeria_Aleksakhina <Virago400@mail.ru>
2022-12-17 14:40:39 +0400

    added Task45

---
+++ Task45/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного копирования.
+
+
+int[] CreateArray()
+{
+int[] array = new int[4];
+for(int i=0; i<4; i++)
+{
+   Console.WriteLine($"Введите {i+1} элемента массива: ");  
+   array[i] = int.Parse(Console.ReadLine()!);
+
+}
+Console.WriteLine(string.Join(" ", array));
+return array;
+}
+void NewArray(int[] Array)
+{
+
+int[] newArr = new int[Array.Length];
+for (int i=0; i<newArr.Length; i++)
+{
+    newArr[i] = Array[i];
+    Console.Write($"{newArr[i]}");
+}
+
+}
+NewArray(CreateArray());
