0aa70f128565eb3ca30bad0da326f7491f376985
Valeria_Aleksakhina <Virago400@mail.ru>
2022-12-20 12:03:46 +0400

    added Task49

---
+++ Task49/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых 
+// оба индекса чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+int[,] CreateNormalMatrix(int a, int b)
+{
+int[,] matrix = new int[a, b];
+for (int rows = 0; rows < matrix.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < matrix.GetLength(1); columns++)
+    {
+        matrix[rows, columns] = new Random().Next(1,10);
+        Console.Write($"{matrix[rows, columns]} ");
+    }
+    Console.WriteLine("");
+}
+Console.WriteLine("");
+return matrix;
+}
+
+void CreateSqrtMatrix(int[,] array)
+{
+int[,] newArray = new int[array.GetLength(0), array.GetLength(1)];
+for (int rows = 0; rows < array.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < array.GetLength(1); columns++)
+    {
+        if(rows%2==0 && columns%2==0) newArray[rows, columns] = (int)Math.Pow(array[rows, columns], 2);
+        else newArray[rows, columns] = array[rows, columns];
+        Console.Write($"{newArray[rows, columns]} ");
+    }
+    Console.WriteLine("");
+}
+}
+CreateSqrtMatrix(CreateNormalMatrix(4,4));
