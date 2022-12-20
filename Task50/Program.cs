c06cbcb0e3912c4117b273b1f331170e2c6ddeb0
Valeria_Aleksakhina <Virago400@mail.ru>
2022-12-20 16:02:00 +0400

    added Task50

---
+++ Task50/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+// в двумерном массиве, и возвращает значение этого элемента или же указание, 
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// [1,7] -> такого числа в массиве нет ([1,7] это позиция по индексам, 1- строка и 7 -столбец)
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+Console.WriteLine("Введите индекс строки массива row: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите индекс столбца массива column: ");
+int column = int.Parse(Console.ReadLine()!);
+
+void FindNum(int[,] array)
+{
+if (row < array.GetLength(0) && column < array.GetLength(1)) 
+Console.Write($"По указанным индексам стоит число ->> {array[row, column]}");
+else Console.WriteLine("Такого элемента в массиве нет");
+}
+
+
+FindNum(array);
+
+
+
+
