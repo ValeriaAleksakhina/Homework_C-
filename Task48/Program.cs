4d613c408d95e2bea4d2869fc7acc3386c68252b
Valeria_Aleksakhina <Virago400@mail.ru>
2022-12-20 11:56:11 +0400

    Task48

---
+++ Task48/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент
+// в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный
+// массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int[,] CreateNormalMatrix(int a, int b)
+{
+int[,] matrix = new int[a, b];
+for (int rows = 0; rows < matrix.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < matrix.GetLength(1); columns++)
+    {
+        matrix[rows, columns] = new Random().Next(1,10); //взять из строки 31
+        Console.Write($"{matrix[rows, columns]} ");
+    }
+    Console.WriteLine("");
+}
+Console.WriteLine("");
+return matrix;
+}
+
+void CreateSpecArray(int [,] array)
+{
+for (int rows = 0; rows < array.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < array.GetLength(1); columns++)
+    {
+        array[rows, columns] = rows + columns;
+        Console.Write($"{array[rows, columns]} ");
+    }
+    Console.WriteLine("");
+}
+}
+CreateSpecArray(CreateNormalMatrix(3,4));
+
+
+
+
