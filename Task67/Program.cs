23454a0e42b6f90df2487e8f29a80353150fb6ce
Valeria_Aleksakhina <Virago400@mail.ru>
2022-12-25 12:18:09 +0400

    added Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 67: Напишите программу, которая будет 
+//принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int GetSumNumb(int a)
+{
+    if (a > 0) a = a % 10 + GetSumNumb(a / 10);
+    return (a);
+}
+Console.WriteLine("Введите число a: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write(GetSumNumb(n));
+
