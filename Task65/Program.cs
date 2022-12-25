6fb8d1fc9838f92e4e5bab2bfcdf2760be6455fb
Valeria_Aleksakhina <Virago400@mail.ru>
2022-12-25 11:51:13 +0400

    added Task65

---
+++ Task65/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от N до M.
+
+void NumbersFromMtoN(int m, int n)
+{
+    if (n > m) NumbersFromMtoN(m, n - 1);
+    Console.Write($"{n} ");
+    if (m > n) NumbersFromMtoN(m, n + 1);
+
+}
+NumbersFromMtoN(10, 1);
