8fd2b2834d449622261c8e7203442cf12dfe4f5c
Valeria_Aleksakhina <Virago400@mail.ru>
2022-12-25 12:29:31 +0400

    added Task69

---
+++ Task69/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 69: Напишите программу, которая на вход 
+// принимает два числа A и B, и возводит число А 
+// в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+int Degree(int a, int b)
+{
+    if (b==0) return 1;
+    else return a * Degree(a, b-1);
+}
+Console.WriteLine("Введите число a: ");
+int n = Math.Abs(int.Parse(Console.ReadLine()!));
+Console.WriteLine("Введите число b: ");
+int m = Math.Abs(int.Parse(Console.ReadLine()!));
+
+Console.WriteLine(Degree(n,m));
