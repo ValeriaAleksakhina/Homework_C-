664d13aebe76c3c900058736f4683e3522b99465
Valeria_Aleksakhina <Virago400@mail.ru>
2022-12-25 14:57:58 +0400

    added Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N.
+//  Напишите программу, которая найдёт сумму 
+//  натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число m:");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число n:");
+int n = int.Parse(Console.ReadLine()!);
+
+void SumNums(int m, int n, int sum)
+{
+    if (m > n) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;1
+    }
+    sum = sum + (m++);
+    SumNums(m, n, sum);
+}
+
+SumNums(m, n, 0);
